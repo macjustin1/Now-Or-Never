25074d045600b87c017a9aa153897267ad54cbc5
macjustin1 <macjustin1@aol.com>
2016-05-19 17:19:45 -0700

    Implemented CloudKit Database to app

--- Now Or Never 2.0/profileViewController.swift
+++ Now Or Never 2.0/profileViewController.swift
@@ -21,7 +21,7 @@ class profileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+        
         self.imageView?.image = self.image
         self.nameLabel?.text = self.name
         self.bioView?.text = self.bio
